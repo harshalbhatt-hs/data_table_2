3ca2a25d6c60b36c338eb85fc92f7ad315f227d4
harshal.bhatt <harshal.bhatt@heliossolution.co>
2024-05-23 19:13:42 +0530

    available row per page condition improved

--- lib/src/paginated_data_table_2.dart
+++ lib/src/paginated_data_table_2.dart
@@ -820,10 +820,7 @@ class PaginatedDataTable2State extends State<PaginatedDataTable2> {
     final List<Widget> footerWidgets = <Widget>[];
 
     if (widget.onRowsPerPageChanged != null) {
-      final List<Widget> availableRowsPerPage = widget.availableRowsPerPage
-          .where((int value) =>
-              value == _effectiveRowsPerPage)
-          .map<DropdownMenuItem<int>>((int value) {
+      final List<Widget> availableRowsPerPage = widget.availableRowsPerPage.map<DropdownMenuItem<int>>((int value) {
         return DropdownMenuItem<int>(
           value: value,
           child: Text('$value'),
